00de73613d95fd6f32ffb84a26412c9cd1cf840b
elons3 <152450688+elons3@users.noreply.github.com>
2024-07-29 15:15:39 -0600

    Update upload.sh

--- bin/upload.sh
+++ bin/upload.sh
@@ -20,18 +20,27 @@ echo
 #     fi
 # fi
 
+# check for annotations directory
+if [ ! -d "$HOME/segmentation_data/annotations/" ]; then
+    echo "Error: Local directory does not exist."
+    exit 1
+fi
+
 # transfer files to supercomputer
+expect_status=0
 expect << EOF
-spawn rsync -avzP --exclude=".DS_Store" --rsync-path="mkdir -p $HOME/groups/fslg_imagseg/nobackup/archive/segmentation_data && rsync" "$HOME/segmentation_data/annotations/" "$username@ssh.rc.byu.edu:$HOME/groups/fslg_imagseg/nobackup/archive/segmentation_data/"
+spawn rsync -avzP --exclude=".DS_Store" --rsync-path='mkdir -p ~/groups/fslg_imagseg/nobackup/archive/segmentation_data && rsync' "$HOME/segmentation_data/annotations/" "$username@ssh.rc.byu.edu:~/groups/fslg_imagseg/nobackup/archive/segmentation_data/"
 expect "Password:"
 send "$password\r"
 expect "Verification code:"
 send "$twofactor\r"
 expect eof
+catch wait result
+set expect_status [lindex \$result 3]
 EOF
 
-if [ $? -eq 0 ]; then
+if [ $expect_status -eq 0 ]; then
     echo "Files transferred successfully."
 else
-    echo "Something went wrong. Good luck."
+    echo "Error during file transfer. Exit status: $expect_status"
 fi
